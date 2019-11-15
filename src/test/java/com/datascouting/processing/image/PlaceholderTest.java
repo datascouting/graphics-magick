5354ecfd9878eb336cfa9247bc16ef37d8ee2bd1
Michailidis Michael <mmichailidis@datascouting.com>
2019-11-15 15:19:51 +0200

    Added a placeholder test

---
+++ src/test/java/com/datascouting/processing/image/PlaceholderTest.java
@@ -0,0 +1,13 @@
+package com.datascouting.processing.image;
+
+import org.junit.Test;
+
+import static org.junit.Assert.assertTrue;
+
+public class PlaceholderTest {
+
+    @Test
+    public void isSuccess() throws Exception {
+        assertTrue(true);
+    }
+}
